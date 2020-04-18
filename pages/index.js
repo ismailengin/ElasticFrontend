f201d7f2c00a8ab4a97b5a10fcdaee32bbfd9343
gleoozhk <iengintr@gmail.com>
2020-04-18 13:35:02 +0300

    Create table from a rest api and download link

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
 import { useState } from 'react'
+import ResultList from './resultlist'
 const superagent = require('superagent')
 
-function customHandleClick(text){
-    console.log(text)
-}
 
-// const customHandleClick = () => {
-//     superagent.get('https://jsonplaceholder.typicode.com/posts/1').end((err,res) => 
-//     {
-//         console.log(res.body)
-//     }
-    
-//     )
-// }
-
-function HomePage() { 
+function HomePage() {
     const [initialText, setText] = useState(" ")
-    return (
-    <div className="container mx-auto">
-            <input className="border-solid border-gray-400 border-2" type="text" onChange={e => setText(e.target.value)}/>
-            <button onClick={() => customHandleClick(initialText)}>
-                Button
-            </button>
+    const [name, setName] = useState([])
 
+    const customHandleClick = (initialText) => {
+        superagent.get('https://jsonplaceholder.typicode.com/posts').query({ userId: initialText }).end((err, res) => {
+            // console.log(res.body)
+            setName(res.body)
+        })
+    }
+
+    return (
+        <div className="container mx-auto">
+            <div className="text-center">
+                <input className="border-solid border-gray-400 border-2" type="text" placeholder="placeHolderText" onChange={e => setText(e.target.value)} />
+                <button className="px-5" onClick={() => customHandleClick(initialText)}>
+                    Button
+            </button>
+            </div>
+            <ResultList itemFromParent={name} />
         </div>
 
     );
