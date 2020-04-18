cc0206a4914593d3f5bd560286acaa218eeaab5d
gleoozhk <iengintr@gmail.com>
2020-04-18 10:02:20 +0300

    Use useState first time

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-const customHandleClick = () => console.log('Free pizza!')
+import { useState } from 'react'
+const superagent = require('superagent')
 
-function HomePage() {
+function customHandleClick(text){
+    console.log(text)
+}
+
+// const customHandleClick = () => {
+//     superagent.get('https://jsonplaceholder.typicode.com/posts/1').end((err,res) => 
+//     {
+//         console.log(res.body)
+//     }
+    
+//     )
+// }
+
+function HomePage() { 
+    const [initialText, setText] = useState(" ")
     return (
     <div className="container mx-auto">
-            <input className="border-solid border-gray-400 border-2" type="text" />
-            <button onClick={customHandleClick}>
+            <input className="border-solid border-gray-400 border-2" type="text" onChange={e => setText(e.target.value)}/>
+            <button onClick={() => customHandleClick(initialText)}>
                 Button
             </button>
 
