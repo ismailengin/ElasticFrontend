b99e0ed98eee491a47495bf4f9c842e68092c22c
ismailengin <iengintr@gmail.com>
2021-08-29 11:06:18 +0300

    Minor

--- pages/api/db.js
+++ pages/api/db.js
@@ -3,6 +3,5 @@ import {query} from '../../lib/db';
 
 export default async function handler(req, res) {
     var results =  await query(req.body).then(data => {console.log(data);return data})
-    console.log(JSON.stringify(results))
     res.status(200).json(results);
 }
